dff9689a706ddf389a4d74eaee94e96512534873
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-12 16:28:50 +0100

    dev: added navigation via typescript

--- front/src/app/stock/routes/add/add.component.ts
+++ front/src/app/stock/routes/add/add.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, ReactiveFormsModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss',
 })
 export class AddComponent {
+  f = new FormGroup({
+    name: new FormControl(),
+    price: new FormControl(),
+    qty: new FormControl(),
+  });
   faPlus = faPlus;
+
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {}
+
+  submit() {
+    console.log('submit');
+    this.router.navigate(['..'], { relativeTo: this.route });
+  }
 }
