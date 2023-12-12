f52b2ad13e0dd145df48ceadfcad930fd5150b89
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-12 11:28:02 +0100

    dev: added routing

---
+++ front/src/app/routes/legal/legal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal',
+  standalone: true,
+  imports: [],
+  templateUrl: './legal.component.html',
+  styleUrl: './legal.component.scss'
+})
+export class LegalComponent {
+
+}
