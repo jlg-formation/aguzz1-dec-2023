fde3936119b1c899f5ff1556a813ca511fcbd7c3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-12 14:03:07 +0100

    dev: added stock list dom and css

--- front/src/app/stock/routes/list/list.component.ts
+++ front/src/app/stock/routes/list/list.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
-export class ListComponent {
-
-}
+export class ListComponent {}
