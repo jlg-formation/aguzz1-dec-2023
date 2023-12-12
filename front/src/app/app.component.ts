c43b1f9ceea95eca6ba8d844a5b79e70f3d95f0a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-12 11:13:43 +0100

    dev: footer on bottom

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LayoutModule } from './layout/layout.module';
 
 @Component({
