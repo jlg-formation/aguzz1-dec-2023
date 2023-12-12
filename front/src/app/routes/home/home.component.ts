de1d276ffd7262eca63b2224120b135721c54c57
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-12 15:04:05 +0100

    dev: added fontawesome

--- front/src/app/routes/home/home.component.ts
+++ front/src/app/routes/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FaIconComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  faAngleRight = faAngleRight;
+}
