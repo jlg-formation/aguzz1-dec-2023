dacb6f17916bb46b9be45de0cb883c1120b19b03
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-12 15:21:18 +0100

    dev: added icons to the app

--- front/src/app/stock/routes/add/add.component.ts
+++ front/src/app/stock/routes/add/add.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './add.component.html',
-  styleUrl: './add.component.scss'
+  styleUrl: './add.component.scss',
 })
 export class AddComponent {
-
+  faPlus = faPlus;
 }
--- front/src/app/stock/routes/list/list.component.ts
+++ front/src/app/stock/routes/list/list.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faCircleNotch,
+  faPlus,
+  faRotateRight,
+  faTrashCan,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FaIconComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+  faPlus = faPlus;
+  faRotateRight = faRotateRight;
+  faTrashCan = faTrashCan;
+  faCircleNotch = faCircleNotch;
+}
