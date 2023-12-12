d359129280721a521fe5a0b5aa4d001a39ce54b9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-12 09:45:41 +0100

    dev: added footer component to layout

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ]
 })
 export class LayoutModule { }
