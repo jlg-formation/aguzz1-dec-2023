37cd27d8d0470fff4d4c42eec40841e7b74154e4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-13 12:09:26 +0100

    dev: make required attributes

--- front/src/app/widgets/fluid-button/fluid-button.component.ts
+++ front/src/app/widgets/fluid-button/fluid-button.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import {
+  IconDefinition,
+  faCircleNotch,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-fluid-button',
@@ -12,9 +15,9 @@ import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 export class FluidButtonComponent {
   faCircleNotch = faCircleNotch;
   @Input()
-  icon = faCircleNotch;
-  @Input()
+  icon: IconDefinition | undefined;
+  @Input({ required: true })
   isDoing = false;
-  @Input()
+  @Input({ required: true })
   label = 'To be replaced';
 }
