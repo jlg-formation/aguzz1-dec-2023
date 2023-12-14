6d17a738e1ad5f4b7fd778439eae4389051d0aa0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-14 15:20:15 +0100

    dev: added e2e test

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -1,6 +1,19 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+describe('Test Gestion Stock', () => {
+  it('Add an article and remove it', () => {
+    cy.visit('/');
+    cy.contains('Gestion Stock');
+    cy.contains('Mentions Légales');
+    cy.contains('main a', 'Voir le stock').click();
+    cy.url().should('include', '/stock');
+    cy.get('a[title="Ajouter"]').click();
+    cy.contains('h1', "Ajout d'un article");
+    const name = 'a_' + Cypress._.random(0, 1e6);
+    cy.get('input').eq(0).clear().type(name);
+    cy.get('input').eq(1).clear().type('123.45');
+    cy.get('input').eq(2).clear().type('234');
+    cy.contains('button', 'Ajouter').click();
+
+    cy.get('table tbody tr').contains(name).click();
+    cy.get('button[title="Supprimer"]').click();
+  });
+});
