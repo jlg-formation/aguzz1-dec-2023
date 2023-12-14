9743fc29670edc32e997aa8148319f354b0c45a7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-14 15:35:24 +0100

    dev: added cypress tests

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -15,5 +15,21 @@ describe('Test Gestion Stock', () => {
 
     cy.get('table tbody tr').contains(name).click();
     cy.get('button[title="Supprimer"]').click();
+    cy.get('table tbody tr').contains(name).should('not.exist');
+  });
+
+  it('should not add a black listed article', () => {
+    cy.visit('/');
+    cy.contains('Gestion Stock');
+    cy.contains('Mentions Légales');
+    cy.contains('main a', 'Voir le stock').click();
+    cy.url().should('include', '/stock');
+    cy.get('a[title="Ajouter"]').click();
+    cy.contains('h1', "Ajout d'un article");
+    const name = 'aaa';
+    cy.get('input').eq(0).clear().type(name);
+    cy.get('input').eq(1).clear().type('123.45');
+    cy.get('input').eq(2).clear().type('234');
+    cy.contains('button', 'Ajouter').should('be.disabled');
   });
 });
