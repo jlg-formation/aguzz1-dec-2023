57a023346b80326f144d21bd911cdff98704f482
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-14 10:52:44 +0100

    dev: typings are already checked
    with angular language service vscode extension

--- front/src/app/widgets/ellipsis.pipe.ts
+++ front/src/app/widgets/ellipsis.pipe.ts
@@ -5,14 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class EllipsisPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): string {
-    if (typeof value !== 'string') {
-      throw new Error('oups');
-    }
-    let maxLength = 10;
-    if (typeof args[0] === 'number') {
-      maxLength = args[0];
-    }
+  transform(value: string, maxLength = 10): string {
     if (value.length > maxLength) {
       return value.substring(0, maxLength) + '...';
     }
